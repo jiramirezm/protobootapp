db7f9779ffcbf2a516b5f85963eb5faad28d3721
JORGE IVAN RAMIREZ MEJIA <unijir@gmail.com>
2024-09-22 20:55:19 -0500

    comando deprecado

--- src/test/java/co/edu/eafit/protobootapp/HttpRequestTest.java
+++ src/test/java/co/edu/eafit/protobootapp/HttpRequestTest.java
@@ -14,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 
+    @SuppressWarnings("deprecation")
     @LocalServerPort
     private int port;
 
